d19f007a23b1d55b2d73de6c8ae17e915a7e4f32
enrico <e.bison@tadaweb.com>
2020-11-29 22:19:13 +0100

    scrape feature

--- internal/app/gofinance/foundamentals/goffoundamental.go
+++ internal/app/gofinance/foundamentals/goffoundamental.go
@@ -13,7 +13,8 @@ const (
 	scrapeUrl string = "https://finance.yahoo.com/quote"
 )
 
-func Get(t string) {
+func Get(t string) string {
+	var response string
 	url := fmt.Sprintf("%s/%s", scrapeUrl, strings.ToUpper(t))
 	fmt.Printf("%v\n", url)
 	res, err := http.Get(url)
@@ -33,12 +34,16 @@ func Get(t string) {
 	fmt.Println("ready!!!")
 	doc.Find("script").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
-		p := s.Contents().Text()
-		if strings.Contains(p, "root.App.main") {
-			fmt.Printf("Review %v \n", p)
+		//json_str = html.split('root.App.main =')[1].split(
+		//'(this)')[0].split(';\n}')[0].strip()
+		content := s.Contents().Text()
+		if strings.Contains(content, "root.App.main") {
+			xresponse := strings.Split((strings.Split(content, "(this)")[0]), "root.App.main =")[1]
+			response = strings.Replace(xresponse, ";\n}", "", 1)
 		}
 		//p := s.Has("root.App.main").Text()
 	})
+	return response
 	// .Each(func(index int, item *goquery.Selection) {
 	// 	title := item.Text()
 	// 	fmt.Println("Title ", title)
--- internal/app/handler/gofhandler.go
+++ internal/app/handler/gofhandler.go
@@ -1,12 +1,24 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/enbis/gofinance/internal/app/gofinance/foundamentals"
 )
 
+type responsePayload struct {
+	Data string `json:"data"`
+}
+
 func Request(w http.ResponseWriter, r *http.Request) {
 	t := r.URL.Query().Get("data")
-	foundamentals.Get(t)
+	data := foundamentals.Get(t)
+
+	w.Header().Add("Content-Type", "application/json")
+
+	// Prepare body
+	json.NewEncoder(w).Encode(responsePayload{
+		Data: data,
+	})
 }
